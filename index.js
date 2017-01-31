f8f005b97c28aaf6383b1e0992114ce0ceef89dc
David Weinstein <txea@Davids-MacBook-Air.local>
2017-01-31 14:28:06 -0500

    done done done

--- index.js
+++ index.js
@@ -1,9 +1,57 @@
+// class OlderCoaster extends React.Component {
+//   render() {
+//     return React.createElement('div', { className: 'oldercoaster' }, [
+//       React.createElement('p', {}, 'Two grannies having the time of their life!'),
+//       React.createElement('p', {}, 'Passengers:'),
+//       React.createElement('ul', {}, [
+//       React.createElement('li', {}, 'Agnes'),
+//       React.createElement('li', {}, 'Muriel'),
+//     ])
+//   ])
+//   }
+// }
+
+const OlderCoaster = React.createClass({
+  render() {
+    return React.createElement('div', { className: 'oldercoaster' }, [
+      React.createElement('p', {}, 'Two grannies having the time of their life!'),
+      React.createElement('p', {}, 'Passengers:'),
+      React.createElement('ul', {}, [
+      React.createElement('li', {}, 'Agnes'),
+      React.createElement('li', {}, 'Muriel'),
+    ])
+  ])
+  }
+})
+
+class InFrontOfYou extends React.Component {
+  render() {
+    return React.createElement('div', {},[
+      React.createElement('p', {}, "You shouldn't look too far."),
+      React.createElement('p', {}, ["Sometimes, the solution is ",
+      React.createElement('strong', {}, "right in front of you.")
+    ])
+    ])
+  }
+}
+
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
   'Short ribs',
   'Beef shin',
   'Ribeye'
 ];
+
+const ButcherShop = React.createClass({
+  render() {
+    return React.createElement('div', {className: 'butcher-shop'}, [
+      React.createElement('p', {}, "Hello! We have the following products for sale today:"),
+      React.createElement('ul', {}, [
+      BUTCHER_PRODUCTS.map((item) => React.createElement('li', {}, item)),
+      ])
+    ])
+  }
+});
 
 ReactDOM.render(
   React.createElement('div', {}, [
